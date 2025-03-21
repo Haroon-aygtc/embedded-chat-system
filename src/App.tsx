a6ab355b07e8a381deeb0a4bb506b2b8b17a51f3
Haroon-aygtc <badshah.dev@outlook.com>
2025-03-21 20:14:37 +0000

    app setting

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
         <Suspense
           fallback={
             <div className="flex items-center justify-center min-h-screen">
-              <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-primary"></div>
+              <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2  border-primary"></div>
             </div>
           }
         >
